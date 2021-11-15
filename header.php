074e6936488efa07a0b3c2df4ad58916e990973e
bsarasuspe <83415989+bsarasuspe@users.noreply.github.com>
2021-11-15 13:34:32 +0100

    Initial commit

---
+++ header.php
@@ -0,0 +1,21 @@
+<!DOCTYPE html>
+	<head>
+		<title>Liburutegia</title>
+		<meta charset=UTF-8">
+		<link rel='stylesheet' type='text/css' href='styles.css'/>
+		<link rel="preconnect" href="https://fonts.googleapis.com">
+		<link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+		<link href="https://fonts.googleapis.com/css2?family=Lato:wght@700&family=Roboto&display=swap" rel="stylesheet">
+	</head>
+	<body>
+		<div id="header">
+			<div id="headertext">LIBURUTEGIA</div>
+		</div>
+		<div id="nav">
+			<ul>
+			  <li><a href="#home">Hasiera</a></li>
+			  <li><a href="#news">Logeatu</a></li>
+			  <li><a href="#contact">Erregistratu</a></li>
+			</ul>
+		</div>
+	</body>
